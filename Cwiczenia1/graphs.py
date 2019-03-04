af83aa9cb9efcedf00f7a95ae4505fb507213bde
michalkruk <michal.k.kruk@gmail.com>
2019-03-04 20:54:25 +0100

    Add solutions to all tasks

--- Cwiczenia1/graphs.py
+++ Cwiczenia1/graphs.py
@@ -1,21 +1,51 @@
-
-
 lines = [line.rstrip('\n') for line in open('graph.txt')]
 levels = []
 matrix = []
+edges = []
+consequent = []
 counter = 0
 
 for i in lines:
     levels.append(i.count('1'))
     matrix.append(i.split(' '))
 
-print ("Stopnie kolejnych wiercholkow: ")
-for idx,i in enumerate(levels):
-    print (idx, i)
+# Zadanie 1
+print("Stopnie kolejnych wiercholkow: ")
+for idx, i in enumerate(levels):
+    print(idx, i)
 
-
-
-print ("Wierzcholki o przylegajace do wierzcholka o najwyzszym stopniu: ")
-for idx,i in enumerate(max(matrix)):
+# Zadanie 2
+print("Wierzcholki o przylegajace do wierzcholka o najwyzszym stopniu: ")
+for idx, i in enumerate(max(matrix)):
     if i == '1':
-        print (idx)
+        print(idx)
+
+# Zadanie 3
+print("Wypisuje, między jakimi wierzcholkami sa krawedzie: ")
+for idx, s in enumerate(matrix):
+    for inx, n in enumerate(s):
+        if (int(idx) <= int(inx)):
+            if (n == '1'):
+                edges.append((idx, inx))
+print(edges)
+
+# Zadanie 4
+w, h = len(matrix), len(edges)
+incidentMatrix = [[0 for x in range(w)] for y in range(h)]
+
+print("Wypisuje macierz incydencji: ")
+for i in enumerate(edges):
+    incidentMatrix[i[0]][i[1][0]]=1
+    incidentMatrix[i[0]][i[1][1]]=1
+print(incidentMatrix)
+
+# Zadanie 5
+
+print("Wypisuje liste następników: ")
+for idx, i in enumerate(matrix):
+    print("Następniki dla ",idx," : ")
+    for inx, n in enumerate(i):
+        if  n =='1':
+            print(inx)
+
+
