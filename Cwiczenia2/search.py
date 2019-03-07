9e5ba5054820087ffe811f09dabdd97d3d854ab7
michalkruk <michal.k.kruk@gmail.com>
2019-03-07 18:03:06 +0100

    Update search for consequent of node

--- Cwiczenia2/search.py
+++ Cwiczenia2/search.py
@@ -1,16 +1,38 @@
-def dfs(graph, node):
-    visited = [node]
-    stack = [node]
+lines = [line.rstrip('\n') for line in open('matrixDFS.txt')]
+matrix = []
+temp = []
+thisdict={}
+
+
+
+
+def dfs_iterative(graph, start):
+    stack, path = [start], []
+
     while stack:
-        node = stack[-1]
-        if node not in visited:
-            visited.extend(node)
-        remove_from_stack = True
-        for next in graph[node]:
-           if next not in visited:
-              stack.extend(next)
-              remove_from_stack = False
-              break
-        if remove_from_stack:
-            stack.pop()
-    return visited
+        vertex = stack.pop()
+        print vertex
+        if vertex in path:
+            continue
+        path.append(vertex)
+        for neighbor in graph[vertex]:
+            stack.append(neighbor)
+
+    return path
+
+for i in lines:
+    matrix.append(i.split(' '))
+
+print("Wypisuje liste nastepnikow: ")
+for idx, i in enumerate(matrix):
+    del temp[:]
+    for inx, n in enumerate(i):
+        if n == '1':
+            temp.append(inx)
+    thisdict[idx] = temp[:]
+
+
+
+print dfs_iterative(thisdict, 0)
+
+print thisdict
